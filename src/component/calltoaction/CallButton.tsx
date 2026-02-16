71aef59af77ece06ebd16d9ebcba463166ff9fab
Ubuntu <ubuntu@ip-172-31-19-159.ap-south-1.compute.internal>
2026-02-16 06:35:10 +0000

    update no

--- src/component/calltoaction/CallButton.tsx
+++ src/component/calltoaction/CallButton.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import React from 'react';
@@ -7,7 +8,7 @@ const CallButton: React.FC = () => {
   return (
     <div className="fixed right-4 bottom-10 z-50">
       <a
-        href="tel:+919999999999"
+        href="tel:+919312511015"
         className="flex items-center justify-center w-14 h-14 md:w-16 md:h-16 rounded-full bg-red-600 text-white shadow-lg animate-pulse hover:scale-105 transition-transform duration-300"
         aria-label="Call Now"
       >
