f649003094de77ec3983863635c07a1a641b78d1
Rahul <codersvox@gmail.com>
2026-02-27 15:06:30 +0530

    Fixed comming soon image

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -1,14 +1,21 @@
-'use client'
-import Image from 'next/image'
-import React from 'react'
-
+"use client";
+import Image from "next/image";
+import React from "react";
 
 export default function ComingSoon() {
-    return (
-        <>                <div className='container  mx-auto my-0 px-2 -mt-14 md:mt-3 md:px-0'>
-            <Image src="/img/coming-soon.jpg" width={1920} height={500} alt='About Us' className='rounded-xl' />
+  return (
+    <>
+      <div className="container mx-auto px-2 mt-2 md:mt-2 md:px-0">
+        <div className="max-w-7xl pb-3 mx-auto">
+          <Image
+            src="/img/coming-soon.jpg"
+            width={1600}
+            height={500}
+            alt="About Us"
+            className="rounded-xl w-full h-auto object-cover"
+          />
         </div>
-
-        </>
-    );
+      </div>
+    </>
+  );
 }
