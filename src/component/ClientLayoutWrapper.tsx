b90d9c3f8c54f8c7133c2ae7ca5cfbb4b948aee4
Rahul <therahulray@gmail.com>
2026-05-18 12:32:23 +0530

    update layout file code

--- src/component/ClientLayoutWrapper.tsx
+++ src/component/ClientLayoutWrapper.tsx
@@ -3,31 +3,38 @@
 import { usePathname } from "next/navigation";
 import Header from "@/component/Header";
 import Footer from "@/component/Footer";
-import WhatsAppMessage from "@/component/calltoaction/WhatsAppMessage";
-import CallButton from "@/component/calltoaction/CallButton";
-import { useAuthStore } from "@/lib/store/auth.store";
-import { SocketProvider } from "@/lib/socket";
 
 export default function ClientLayoutWrapper({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { user } = useAuthStore();
+
   const pathname = usePathname();
 
-  const isAdmin = pathname?.startsWith("/admin");
-  const isDashboard = pathname?.startsWith("/dashboard");
+  const isAdmin =
+    pathname?.startsWith("/admin");
+
+  const isDashboard =
+    pathname?.startsWith("/dashboard");
 
   return (
-    <SocketProvider userId={user?.id}>
-      {!isAdmin && !isDashboard && <Header />}
+
+    <>
+
+      {!isAdmin &&
+        !isDashboard &&
+        <Header />
+      }
 
       {children}
 
-      {!isAdmin && <Footer />}
-      {!isAdmin && <WhatsAppMessage />}
-      {!isAdmin && <CallButton />}
-    </SocketProvider>
+      {!isAdmin &&
+        <Footer />
+      }
+
+    </>
+
   );
+
 }
